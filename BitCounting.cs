b908528deee7ff0479a22264e2dcc13b90471d03
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-06-12 19:35:30 +0300

    Finished Bit Counting

---
+++ BitCounting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class BitCounting
+    {
+        public static int CountBits(int n)
+        {
+            int counter = 0;
+            foreach ( char elem in (Convert.ToString(n, 2).Trim('0')).ToCharArray())
+            {
+                if (elem == '1') counter++;
+            }
+            return counter;
+        }
+    }
+}
