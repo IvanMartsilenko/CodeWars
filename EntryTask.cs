21fe3b51b4126d3ddb4b90e9676b22a619ee391b
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-06-12 18:42:18 +0300

    Finished Reversed Words

--- EntryTask.cs
+++ EntryTask.cs
@@ -9,7 +9,5 @@ namespace CodeWars
             return a*b;
         }
     }
-    class EntryTask
-    {
-    }
+   
 }
--- ReverseWords.cs
+++ ReverseWords.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace CodeWars
 {
-    class ReverseWords
+    public class ReverseWords
     {
-        private static string SpinWords(string sentence)
+        public static string SpinWords(string sentence)
         {
             string[] res = sentence.Split(' ');
             string result = "";
             foreach ( string word in res)
             {
-                if(word.Length == 5)
+                if (word.Length >= 5)
                 {
                     char[] wordarray = word.ToCharArray();
-                        Array.Reverse(wordarray);
-                        string newWord = new string(wordarray);
-                    result+= newWord;
+                    Array.Reverse(wordarray);
+                    string newWord = new string(wordarray);
+                    result += newWord + " ";
                 }
-                result += word;
+                else
+                {
+                    result +=  word + " ";
+                }
             }
+            result = result.TrimEnd(' ');
             return result;
         }
 
-
+    
     }
 }
