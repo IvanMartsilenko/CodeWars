603ed833287ca0b7f261d19e1149a404a4235aff
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-06-12 17:40:28 +0300

    Added function SpinWords

---
+++ EntryTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class CustomMath
+    {
+        public static int multiply(int a, int b)
+        {
+            return a*b;
+        }
+    }
+    class EntryTask
+    {
+    }
+}
---
+++ ReverseWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class ReverseWords
+    {
+        private static string SpinWords(string sentence)
+        {
+            string[] res = sentence.Split(' ');
+            string result = "";
+            foreach ( string word in res)
+            {
+                if(word.Length == 5)
+                {
+                    char[] wordarray = word.ToCharArray();
+                        Array.Reverse(wordarray);
+                        string newWord = new string(wordarray);
+                    result+= newWord;
+                }
+                result += word;
+            }
+            return result;
+        }
+
+
+    }
+}
