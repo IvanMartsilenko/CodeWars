54909af61d07fb9090c6873af169cbcd95cb5480
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-06-12 17:18:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class CustomMath
+    {
+        public static int multiply(int a, int b)
+        {
+            return a*b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res = CustomMath.multiply(1, 2);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
