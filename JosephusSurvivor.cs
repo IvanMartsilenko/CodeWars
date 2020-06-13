2186e8fc9632ddb72f2fc4303241438ee25dff2a
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-06-13 18:27:35 +0300

    Finished JosephusSurvivor

---
+++ JosephusSurvivor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class JosephusSurvivor
+    {
+        public static int JosSurvivor(int n, int k)
+        {
+            Queue<int>  _children = new Queue<int>(Enumerable.Range(1, n));
+            while(_children.Count != 1)
+            {
+                for( int i = 1; i < k; i++) { _children.Enqueue(_children.Dequeue()); }
+                _children.Dequeue();
+            }
+            return _children.Peek();
+        }
+    }
+}
